e0b24e8b761e40af87ead193c66351e8b748be5f
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.com>
2022-04-28 17:29:20 +0200

    update js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -4,18 +4,11 @@ function updateHeadline(title, picture, content) {
     document.getElementById('headlineContent').innerHTML = content;
 }
 
-updateHeadline(
-    'Default Title',
-    'assets/images/default_image.png',
-    'Sit fugiat aute aute magna. Voluptate adipisicing ullamco nostrud esse. Exercitation fugiat consequat sunt aliquip adipisicing proident nulla deserunt aute ut fugiat. Laboris cillum qui sit in adipisicing quis irure nostrud quis veniam. Qui et id non culpa culpa cillum reprehenderit ut mollit.Labore occaecat enim cupidatat cupidatat enim adipisicing dolor officia elit deserunt. Lorem culpa sint tempor nostrud ipsum aliqua veniam ipsum do aute proident. Do fugiat consectetur tempor adipisicing amet officia non in voluptate. Ea aliqua fugiat adipisicing Lorem exercitation eiusmod pariatur deserunt amet et dolore exercitation consequat.'
-);
-
 document.getElementById('changeHeadlineButton').addEventListener('click', function () {
     //TODO 1 : Get a random article
 });
 
-
-document.getElementById('searchHeadline').addEventListener('change', function(e) {
+document.getElementById('searchHeadline').addEventListener('input', function(e) {
     //Here we get the value typed in the input
     let search = e.target.value;
 
